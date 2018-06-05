9a3b7bd36d79098f26e452ceb1bb8688b553aabb
xMalware <xmalware2@gmail.com>
2018-06-05 15:29:53 +0200

    Added log actions

--- BadblockAPI/src/main/java/common/fr/badblock/api/common/utils/logs/Log.java
+++ BadblockAPI/src/main/java/common/fr/badblock/api/common/utils/logs/Log.java
@@ -1,15 +1,29 @@
 package fr.badblock.api.common.utils.logs;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Log 
 {
 
-	//private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyy/MM/dd HH:mm:ss");
+	private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyy/MM/dd HH:mm:ss");
 
+	private static LogAction	logAction;
+	
+	public static void setAction(LogAction action)
+	{
+		logAction = action;
+	}
+	
 	public static String log(LogType logType, String message) 
 	{
-		//String date = simpleDateFormat.format(new Date());
-		//String result = date + " " + logType.getChatColor() + "[" + logType.name() + "] " + ChatColor.RESET + message;
+		String date = simpleDateFormat.format(new Date());
+		String result = date + " " + logType.getChatColor() + "[" + logType.name() + "] " + ChatColor.RESET + message;
 		System.out.println(message);
+		if (logAction != null)
+		{
+			logAction.done(result);
+		}
 		return message;
 	}
 
---
+++ BadblockAPI/src/main/java/common/fr/badblock/api/common/utils/logs/LogAction.java
@@ -0,0 +1,8 @@
+package fr.badblock.api.common.utils.logs;
+
+public abstract class LogAction
+{
+
+	public abstract void done(String message);
+	
+}
