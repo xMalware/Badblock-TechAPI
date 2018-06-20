8135c2cf0e6054fce75f604897bf1047a7c14289
xMalware <xmalware2@gmail.com>
2018-06-20 23:29:43 +0200

    BadblockAPI

---
+++ BadblockAPI/src/main/java/common/fr/badblock/api/BadblockAPI.java
@@ -0,0 +1,16 @@
+package fr.badblock.api;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BadblockAPI extends JavaPlugin
+{
+	
+	@Override
+	public void onLoad()
+	{
+		// Load the server
+	}
+	
+	
+	
+}
